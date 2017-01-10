eefc6aca8991ab32bbe0755d762b4d92e085da52
Anna Lotko <annaklotko@gmail.com>
2017-01-10 15:39:12 -0700

    videos and carousel populates

--- public/js/projects/project-page.component.js
+++ public/js/projects/project-page.component.js
@@ -7,16 +7,37 @@
       templateUrl: "js/projects/project-page.template.html"
     });
 
-    function ProjectPage() {
+    function ProjectPage($sce) { //
+      console.log($sce);
       const vm = this;
+
+      // Params for the carousel
       vm.slides = [];
-      // vm.currIndex = 0;
       vm.myInterval = 3000;
       vm.noWrapSlides = false;
       vm.active = 0;
 
       vm.$onInit = function() {
 
+        // GET ROUTE FOR THIS ID
+        // HOW DOES IT KNOW WHAT THE ID IS? (URL ROUTE)
+        // Create project object
+        vm.project = {
+          id: 1,
+          name: "Fish Tank",
+          role: "Designer",
+          date: "Today",
+          brief: "A fishtank",
+          description: "Have you ever wondered what it would be like to be a crustacean? I did, so I made Fishtank: a crustacean simulator. The controls are slow, which I imagine reflect reality is some way. The controls for fishtank are simple: WASD to move, mouse to look. If you get a chance to download the build and play, you’ll notice how much effort it takes to look around. This was a deliberate detail, which contributes to the feeling of living in a fishtank.",
+          tags: ["this", "that"],
+          live_url: "google.com",
+          videos: []
+        };
+
+        vm.project.videos.push({link: $sce.trustAsResourceUrl('https://www.youtube.com/embed/AvjiAWshMAk')});
+        vm.project.videos.push({link:$sce.trustAsResourceUrl('https://www.youtube.com/embed/AvjiAWshMAk')});
+
+        // Populate with images
         vm.slides = [
           {image: 'https://s3-us-west-2.amazonaws.com/popuparcade.com/Fishtank_IMG/Portfolio_IMG_FT_01.png',
            id: 0,
@@ -27,24 +48,6 @@
            id: 1,
            text: 'Hello'
          }];
-
-        //  for (var i = 0; i < 4; i++) {
-        //    vm.addSlide();
-        //  }
-
-      // setTimeout(function() {
-      //   $('.project-demo').slick({
-      //     dots: true,
-      //     infinite: true,
-      //     speed: 500,
-      //     fade: true,
-      //     cssEase: 'linear',
-      //     autoplay: true,
-      //     autoplaySpeed: 2000,
-      //     prevArrow: false,
-      //     nextArrow: false
-      //   });
-      // }, 2000 );
      };
    }
 })();
