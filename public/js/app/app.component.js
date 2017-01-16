737a94d681221831c346459de496e76cb4e7e391
Anna Lotko <annaklotko@gmail.com>
2017-01-16 15:35:38 -0700

    updated navbar to be responsive with bootstrap UI

--- public/js/app/app.component.js
+++ public/js/app/app.component.js
@@ -8,8 +8,11 @@
 
     controller.$inject = ['$http', '$state', '$stateParams'];
 
-    function controller($http) {
+    function controller($http, $state, $stateParams) {
       const vm = this;
+      vm.isNavCollapsed = true;
+      // vm.isCollapsed = false;
+      // vm.isCollapsedHorizontal = false;
 
       vm.$onInit = function() {
       };
