162e0c8efdc805701035ec43b2b04a44d1bdeb2b
Anna Lotko <annaklotko@gmail.com>
2017-01-17 16:25:32 -0700

    added about and home text / updated card border-radius

--- public/js/archive/archive-list.component.js
+++ public/js/archive/archive-list.component.js
@@ -11,57 +11,34 @@
 
     function controller($http, $state, $stateParams) {
       const vm = this;
+      vm.activeFilter = "all";
+      var archiveData = [];
+      
+      vm.updateFilter = function(newFilter) {
+        vm.activeFilter = newFilter;
+        // Clear archive and only display projects for this filter
+        vm.archive = [];
+        if (newFilter === "all") {
+          vm.archive = archiveData;
+        } else {
+          for (var i = 0; i < archiveData.length; i++) {
+            if (archiveData[i].type === newFilter) {
+              vm.archive.push(archiveData[i]);
+            }
+          }
+        }
+      };
 
       vm.selectProject = function (projectId) {
-        console.log(projectId);
         $state.go("projectpage", { projectId: projectId});
       };
 
       vm.$onInit = function() {
         $http.get('/archive').then(function (response) {
+          archiveData = response.data;
           vm.archive = response.data;
           console.log(vm.archive);
         });
-
-      //   vm.archive = [
-      //   {
-      //   thumb: "https://s3-us-west-2.amazonaws.com/popuparcade.com/Thumbs/Gloobs_Thumb.png",
-      //   name: "Gloobal Doomination",
-      //   brief: "In Gloobal Doomination, you control a population of Gloobs—friendly but ultimately doomed creatures that have no idea what kind of horrible disaster awaits them.",
-      //   kind: "game"
-      //   },
-      //   {
-      //   thumb: "https://s3-us-west-2.amazonaws.com/popuparcade.com/Thumbs/Gloobs_Thumb.png",
-      //   name: "Gloobal Doomination",
-      //   brief: "In Gloobal Doomination, you control a population of Gloobs—friendly but ultimately doomed creatures that have no idea what kind of horrible disaster awaits them.",
-      //   kind: "game"
-      //   },
-      //   {
-      //   thumb: "https://s3-us-west-2.amazonaws.com/popuparcade.com/Thumbs/Gloobs_Thumb.png",
-      //   name: "Gloobal Doomination",
-      //   brief: "In Gloobal Doomination, you control a population of Gloobs—friendly but ultimately doomed creatures that have no idea what kind of horrible disaster awaits them.",
-      //   kind: "game"
-      //   },
-      //   {
-      //   thumb: "https://s3-us-west-2.amazonaws.com/popuparcade.com/Thumbs/Gloobs_Thumb.png",
-      //   name: "Gloobal Doomination",
-      //   brief: "In Gloobal Doomination, you control a population of Gloobs—friendly but ultimately doomed creatures that have no idea what kind of horrible disaster awaits them.",
-      //   kind: "game"
-      //   },
-      //   {
-      //   thumb: "https://s3-us-west-2.amazonaws.com/popuparcade.com/Thumbs/Gloobs_Thumb.png",
-      //   name: "Gloobal Doomination",
-      //   brief: "In Gloobal Doomination, you control a population of Gloobs—friendly but ultimately doomed creatures that have no idea what kind of horrible disaster awaits them.",
-      //   kind: "game"
-      //   },
-      //   {
-      //   thumb: "https://s3-us-west-2.amazonaws.com/popuparcade.com/Thumbs/Gloobs_Thumb.png",
-      //   name: "Gloobal Doomination",
-      //   brief: "In Gloobal Doomination, you control a population of Gloobs—friendly but ultimately doomed creatures that have no idea what kind of horrible disaster awaits them.",
-      //   kind: "game"
-      //   }
-      // ];
-
       };
     }
 })();
